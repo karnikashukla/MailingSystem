6f04390b2ccc1e61a2e811a34659a5630caf0aa8
karnika <karnikashukla25@gmail.com>
2020-11-26 21:41:55 +0530

    Admin session

--- src/main/java/com/intranet/mailingsystem/controllers/admin/AdminComposeController.java
+++ src/main/java/com/intranet/mailingsystem/controllers/admin/AdminComposeController.java
@@ -51,83 +51,84 @@ public class AdminComposeController {
 
     @RequestMapping(value = "/compose", consumes = {"multipart/form-data"},method = RequestMethod.POST)
     public String sendAdminEmail(@Valid @ModelAttribute("compose") Mail mail, HttpSession session, ModelMap modelMap, @RequestParam(value = "file") MultipartFile[] multipartFiles) {
+        if (session.getAttribute("adminId") != null) {
+            long id = (long) session.getAttribute("adminId");
 
-        long id = (long) session.getAttribute("adminId");
+            Admin admin = adminService.getAdminById(id);
 
-        Admin admin = adminService.getAdminById(id);
+            String fromMail = admin.getEmail();
 
-        String fromMail = admin.getEmail();
-
-        // Getting current System time
-        LocalDateTime current = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
-        String formatted = current.format(formatter);
+            // Getting current System time
+            LocalDateTime current = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+            String formatted = current.format(formatter);
 
 
+            // Storing file in specific folder
+            formatted = formatted.replaceAll("[^\\w\\s]", " ");
+            String UPLOADED_FOLDER = "E://Intranet/admin/" + admin.getId() + "/" + formatted;
+            List<String> tempFileNames = new ArrayList<String>();
 
-        // Storing file in specific folder
-        formatted = formatted.replaceAll("[^\\w\\s]"," ");
-        String UPLOADED_FOLDER = "E://Intranet/admin/" + admin.getId() + "/" + formatted;
-        List<String> tempFileNames = new ArrayList<String>();
+            if (multipartFiles != null) {
+                File dir = new File(UPLOADED_FOLDER);
+                for (int i = 0; i < multipartFiles.length; i++) {
+                    MultipartFile file = multipartFiles[i];
+                    System.out.println(" File name : " + file.getOriginalFilename());
+                    System.out.println("Is file empty? : " + file.isEmpty());
 
-        if(multipartFiles != null){
-            File dir = new File(UPLOADED_FOLDER);
-            for (int i = 0; i < multipartFiles.length; i++) {
-                MultipartFile file = multipartFiles[i];
-                System.out.println(" File name : " +file.getOriginalFilename());
-                System.out.println("Is file empty? : " + file.isEmpty());
+                    try {
+                        byte[] bytes = file.getBytes();
 
-                try {
-                    byte[] bytes = file.getBytes();
+                        if (!dir.exists())
+                            dir.mkdirs();
 
-                    if (!dir.exists())
-                        dir.mkdirs();
+                        File uploadFile = new File(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(uploadFile));
+                        outputStream.write(bytes);
+                        tempFileNames.add(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
+                        outputStream.close();
 
-                    File uploadFile = new File(dir.getAbsolutePath() + File.separator + file.getOriginalFilename());
-                    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(uploadFile));
-                    outputStream.write(bytes);
-                    tempFileNames.add(dir.getAbsolutePath() + File.separator +  file.getOriginalFilename());
-                    outputStream.close();
-
-                } catch (FileNotFoundException fileNotFoundException) {
-                    System.out.println("Image not Found");
-                    modelMap.addAttribute("imageError", "Please upload a valid file");
-                } catch (IOException ioException) {
-                    System.out.println("Error Occured while saving file");
-                    modelMap.addAttribute("imageError", "There was a problem while saving file. Please try again");
-                } catch (Exception exception) {
-                    exception.printStackTrace();
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        System.out.println("Image not Found");
+                        modelMap.addAttribute("imageError", "Please upload a valid file");
+                    } catch (IOException ioException) {
+                        System.out.println("Error Occured while saving file");
+                        modelMap.addAttribute("imageError", "There was a problem while saving file. Please try again");
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
                 }
             }
-        }
 
 
-        // Extracting multiple emails from toMail and sending mail to each user
-        List<String> toMailList = Arrays.asList(mail.getToMail().split(","));
+            // Extracting multiple emails from toMail and sending mail to each user
+            List<String> toMailList = Arrays.asList(mail.getToMail().split(","));
 
 
-        for(String toMail: toMailList){
-            Mail tempMail = new Mail();
-            if(mailService.searchMail(toMail)){
-                tempMail.setToName(adminService.getAdminByEmail(toMail).getFirstName() + " " + adminService.getAdminByEmail(toMail).getLastName());
+            for (String toMail : toMailList) {
+                Mail tempMail = new Mail();
+                if (mailService.searchMail(toMail)) {
+                    tempMail.setToName(adminService.getAdminByEmail(toMail).getFirstName() + " " + adminService.getAdminByEmail(toMail).getLastName());
+                } else {
+                    tempMail.setToName(userService.getUserByEmail(toMail).getFirstName() + " " + userService.getUserByEmail(toMail).getLastName());
+                }
+
+                tempMail.setDate(formatted);
+                tempMail.setFromName(admin.getFirstName() + " " + admin.getLastName());
+                tempMail.setFromMail(fromMail);
+                tempMail.setSubject(mail.getSubject());
+                tempMail.setBody(mail.getBody());
+                tempMail.setDocuments(tempFileNames);
+
+                tempMail.setToMail(toMail);
+                mailService.save(tempMail);
+                System.out.println("Done");
             }
-            else{
-                tempMail.setToName(userService.getUserByEmail(toMail).getFirstName() + " " + userService.getUserByEmail(toMail).getLastName());
-            }
 
-            tempMail.setDate(formatted);
-            tempMail.setFromName(admin.getFirstName() + " " + admin.getLastName());
-            tempMail.setFromMail(fromMail);
-            tempMail.setSubject(mail.getSubject());
-            tempMail.setBody(mail.getBody());
-            tempMail.setDocuments(tempFileNames);
-
-            tempMail.setToMail(toMail);
-            mailService.save(tempMail);
-            System.out.println("Done");
+            return "redirect:/admin/compose";
+        } else {
+            return "redirect:/admin/login";
         }
-
-        return "redirect:/admin/compose";
     }
 
 }
