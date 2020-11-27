2b79886c7826bb399b9e43524faa5c65a8a4156b
Karan Bhatt <karanbhatt512@gmail.com>
2020-11-27 13:30:18 +0530

    Admin logout URL bind in html pages

--- src/main/java/com/intranet/mailingsystem/MailingsystemApplication.java
+++ src/main/java/com/intranet/mailingsystem/MailingsystemApplication.java
@@ -2,9 +2,15 @@ package com.intranet.mailingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MailingsystemApplication {
+public class MailingsystemApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MailingsystemApplication.class);
+    }
 
     public static void main(String[] args) {
 
